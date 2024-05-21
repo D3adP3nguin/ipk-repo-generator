2da947c6b47b68bc511c395726d0ea9cecd3bcb3
Shant Agopian <penguintesting641@gmail.com>
2024-05-21 11:20:01 -0700

    Modified Manifest creation

--- scripts/generate_manifest.sh
+++ scripts/generate_manifest.sh
@@ -2,15 +2,31 @@
 
 # Function to generate Packages.manifest
 generate_packages_manifest() {
-    packages_file=$1
+    ipk_dir=$1
     manifest_file=$2
 
-    md5sum "$packages_file" > "$manifest_file"
+    > "$manifest_file" # Empty the file
+
+    find "$ipk_dir" -type f -name '*.ipk' | while read -r ipk; do
+        filename=$(basename "$ipk")
+        temp_dir=$(mktemp -d)
+
+        # Extract the data.tar.gz file
+        tar -xOf "$ipk" ./data.tar.gz | tar -xz -C "$temp_dir"
+
+        # List the files and add package metadata
+        echo "Package: ${filename%.*}" >> "$manifest_file"
+        find "$temp_dir" -type f | sed "s|$temp_dir||" >> "$manifest_file"
+        echo "" >> "$manifest_file"
+
+        rm -rf "$temp_dir"
+    done
+
     echo "Packages.manifest file generated at $manifest_file"
 }
 
 # Main script
-packages_file="./output/Packages"
+ipk_dir="./repo/IPK_files"
 manifest_file="./output/Packages.manifest"
 
-generate_packages_manifest "$packages_file" "$manifest_file"
+generate_packages_manifest "$ipk_dir" "$manifest_file"
