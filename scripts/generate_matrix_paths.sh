6134a5b4611e1a6b0256b1ab1ec5805dd99e3c57
Shant Agopian <penguintesting641@gmail.com>
2024-05-28 12:12:42 -0700

    added debug messages

--- scripts/generate_matrix_paths.sh
+++ scripts/generate_matrix_paths.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Debugging: Output the current working directory
+echo "Current directory: $(pwd)"
+
 # Find all relevant directories and output them in a JSON array format
 paths=$(find ../devices -type d -mindepth 3 -maxdepth 3 | jq -R -s -c 'split("\n") | map(select(. != ""))')
+echo "Generated paths: $paths"
+
+# Output paths
 echo "$paths"
