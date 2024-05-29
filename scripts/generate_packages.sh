4f5b7dfd9355ba405994be0f008480c7ef9d77cf
Shant Agopian <penguintesting641@gmail.com>
2024-05-29 12:56:27 -0700

    output repo fix

--- scripts/generate_packages.sh
+++ scripts/generate_packages.sh
@@ -2,8 +2,7 @@
 
 # Hardcoded paths
 OUTPUT_DIR="./output"
-USIGN_PATH="./repo/usign/build/usign"
-CATEGORY=$1
+USIGN_PATH="./output/usign"
 
 # Ensure necessary directories exist
 mkdir -p "$OUTPUT_DIR"
@@ -13,7 +12,6 @@ mkdir -p "$(dirname "$USIGN_PATH")"
 extract_control_from_ipk() {
     ipk_file=$1
     output_file=$2
-    category=$3
     filename=$(basename "$ipk_file")
 
     # Decompress the IPK file (gzip compressed)
@@ -32,26 +30,18 @@ extract_control_from_ipk() {
     fi
 
     # Extract control file from control.tar.gz
-    tar -xzOf control.tar.gz ./control > control
+    tar -xzOf control.tar.gz ./control >> "$output_file"
     if [ $? -ne 0 ]; then
         echo "Error: Failed to extract control file from control.tar.gz"
         rm temp.tar control.tar.gz
         exit 1
     fi
 
-    # Add category tag to the Description field
-    if [ -n "$category" ]; then
-        sed -i "s/^Description:/Description: [$category] /" control
-    fi
-
-    # Append control file content to the output file
-    cat control >> "$output_file"
-
     # Add Filename field
     echo "Filename: $filename" >> "$output_file"
 
     # Cleanup temporary files
-    rm temp.tar control.tar.gz control
+    rm temp.tar control.tar.gz
     echo "" >> "$output_file" # Add an empty line between entries
 }
 
@@ -59,23 +49,21 @@ extract_control_from_ipk() {
 generate_packages() {
     ipk_dir=$1
     output_file=$2
-    category=$3
 
     > "$output_file" # Empty the file
 
     find "$ipk_dir" -type f -name '*.ipk' | while read -r ipk; do
-        extract_control_from_ipk "$ipk" "$output_file" "$category"
+        extract_control_from_ipk "$ipk" "$output_file"
     done
 
     echo "Packages file generated at $output_file"
 }
 
 # Main script
-ipk_dir="./repo/IPK_files"
+ipk_dir="./output"
 packages_file="$OUTPUT_DIR/Packages"
-category="$1"
 
-generate_packages "$ipk_dir" "$packages_file" "$category"
+generate_packages "$ipk_dir" "$packages_file"
 
 # Verify the Packages file is not empty
 if [ ! -s "$packages_file" ]; then
