5930ea8d86046414fd0100eba44db56a5567682f
Shant Agopian <penguintesting641@gmail.com>
2024-05-29 17:25:22 -0700

    modular s3 uplaod script

--- scripts/upload_to_s3.sh
+++ scripts/upload_to_s3.sh
@@ -7,20 +7,15 @@ aws_region="${AWS_REGION}"
 aws_access_key_id="${AWS_ACCESS_KEY_ID}"
 aws_secret_access_key="${AWS_SECRET_ACCESS_KEY}"
 
-# Check if artifacts directory exists
-if [ ! -d "$artifacts_dir" ]; then
-    echo "Error: Artifacts directory $artifacts_dir does not exist."
-    exit 1
-fi
-
 # Configure AWS CLI with the provided credentials
 aws configure set aws_access_key_id "$aws_access_key_id"
 aws configure set aws_secret_access_key "$aws_secret_access_key"
 aws configure set region "$aws_region"
 
-# Organize and upload artifacts to S3
+# Organize and upload artifacts to S3 without the category in the path
 for artifact in "$artifacts_dir"/*; do
-    dest_path="s3://$bucket_name/${artifact##*/}"
+    base_dir=$(basename "$artifact" | sed -E 's/-curated--|-supported--//') # Remove category from directory name
+    dest_path="s3://$bucket_name/$base_dir"
     echo "Uploading $artifact to $dest_path"
     aws s3 cp "$artifact" "$dest_path" --recursive --sse AES256
     if [ $? -ne 0 ]; then
